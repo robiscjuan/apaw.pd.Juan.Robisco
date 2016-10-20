b743fab8a778f5697d096530f7f136143cab66f4
Juan <robiscjuan1994@gmail.com>
2016-10-21 00:34:25 +0200

    Creation (Not working)

---
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumber.java
@@ -0,0 +1,33 @@
+package es.upm.miw.pd.factoryMethod.naturalNumber;
+
+public abstract class NaturalNumber {
+	
+	   private int value;
+
+	    public NaturalNumber(int value) {
+	        this.setValue(value);
+	    }
+
+	    public int getValue() {
+	        return value;
+	    }
+
+	    public void setValue(int value) {
+	        assert value >= 0;
+	        this.value = value;
+	    }
+
+	    public void add(int value) {
+	        this.setValue(this.value + value);
+	    }
+
+	    public String getTextValue() {
+	        if (this.value < getTextValues().length) {
+	            return getTextValues()[this.value];
+	        } else {
+	            return "???";
+	        }
+	    }
+	    
+	    protected abstract String[] getTextValues();
+}
---
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEn.java
@@ -0,0 +1,14 @@
+package es.upm.miw.pd.factoryMethod.naturalNumber;
+
+public class NaturalNumberEn extends NaturalNumber{
+    private static final String[] textValue = {"zero", "one", "two", "three", "four", "five"};
+    
+    public NaturalNumberEn(int value) {
+        super(value);
+    }
+    
+    @Override
+    protected String[] getTextValues() {
+        return NaturalNumberEn.textValue;
+    }
+}
